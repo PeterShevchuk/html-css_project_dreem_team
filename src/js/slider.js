c299b9ee5a0a9ff4e5ecc1e9618f51b247195461
PeterShevchuk <raf02041994@gmail.com>
2020-06-16 17:04:09 +0300

    and

---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+
+
+// $(document).ready(function () {
+//     $('.teachers__list').slick({
+//         slidesToShow: 1,
+//         slideToScroll: 1,
+//         dots: true,
+//         infinite: true,
+//         speed: 300,
+//     });
+// });
