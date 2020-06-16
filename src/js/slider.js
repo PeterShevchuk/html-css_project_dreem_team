c8ffcaf7eb59554cd5e6e38accf2fa73dc345fea
PeterShevchuk <raf02041994@gmail.com>
2020-06-16 23:44:46 +0300

    end teachers block

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,31 +1,35 @@
-
-
 $(document).ready(function () {
-    $('.teachers__list').slick({
-        slidesToShow: 1,
-        slideToScroll: 1,
-        dots: false,
-        infinite: true,
-        speed: 300,
-        mobileFirst: true,
-        // accessibility: false,
-        appendArrows: '.teachers',
-        prevArrow: '<button class="slider__icon-prev"><svg class=""><use xlink: href="../../images/svg/slider/sprite.svg#icon-prev"></use></svg></button>',
-        nextArrow: '<button class="slider__icon-next"><svg class=""><use xlink: href="../../images/svg/slider/sprite.svg#icon-next"></use></svg></button>',
-        responsive: [
-            {
-                breakpoint: 1420,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3,
-                    centerMode: true
-                }
-            }
-        ]
-    });
+  $('.teachers__list').slick({
+    variableWidth: true,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    prevArrow:
+      '<button class="slider__icon-prev"><svg class=""> <use xlink:href="../../images/svg/slider/sprite.svg#icon-prev"></use></svg></button>',
+    nextArrow:
+      '<button class="slider__icon-next"><svg class=""><use xlink:href="../../images/svg/slider/sprite.svg#icon-next"></use></svg></button>',
+    responsive: [
+      {
+        breakpoint: 2000,
+        settings: {
+          centerMode: true,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1420,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
